6990781eab95f3ae67797a6c614e2037555dde86
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-30 17:50:01 -0600

    Init commit

---
+++ src/main/java/com/seminuevos_colima/SeminuevosColimaDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.seminuevos_colima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminuevosColimaDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminuevosColimaDealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seminuevos_colima/SeminuevosColimaDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminuevos_colima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminuevosColimaDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

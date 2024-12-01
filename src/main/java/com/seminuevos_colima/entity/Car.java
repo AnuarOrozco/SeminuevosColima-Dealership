3d5f337a09c919f223356990fa53ef3cff1e1df8
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-30 18:07:47 -0600

    Added the JPA annotations, validations and relations

--- src/main/java/com/seminuevos_colima/entity/Car.java
+++ src/main/java/com/seminuevos_colima/entity/Car.java
@@ -1,33 +1,76 @@
 package com.seminuevos_colima.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "vehiculos")
 public class Car {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_vehiculo")
     private Long id;
+
+    @Column(name = "marca")
     private String brand;
+
+    @Column(name = "modelo")
     private String model;
+
+    @Column(name = "anio")
     private Integer year;
+
+    @Column(name = "ciudad")
     private String city;
+
+    @Column(name = "kilometraje")
     private Integer mileage;
+
+    @Column(name = "es_negociable")
     private Boolean isNegotiable;
+
+    @Column(name = "precio")
     private BigDecimal price;
+
+    @Column(name = "tipo")
     private String type;
+
+    @Column(name = "tiene_aire_acondicionado")
     private Boolean hasAirConditioning;
+
+    @Column(name = "traccion")
     private String traction;
+
+    @Column(name = "color")
     private String color;
+
+    @Column(name = "tipo_de_ventanas")
     private String typeOfWindows;
+
+    @Column(name = "sistema_de_direccion")
     private String steeringSystem;
+
+    @Column(name = "tipo_de_transmicion")
     private String transmissionType;
+
+    @Column(name = "tapiceria")
     private String upholsteryType;
+
+    @Column(name = "combustible")
     private String fuelType;
+
+    @Column(name = "esta_disponible")
     private Boolean isAvailable;
+
+    @ManyToOne
+    private User owner;
+
+    @ManyToMany(mappedBy = "wishlist")
+    private List<User> usersWishlist;
 
 }
--- src/main/java/com/seminuevos_colima/entity/User.java
+++ src/main/java/com/seminuevos_colima/entity/User.java
@@ -1,10 +1,14 @@
 package com.seminuevos_colima.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -12,10 +16,36 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor @NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private Long id;
+
+    @Column(name = "nombre")
     private String name;
+
+    @Email(message = "El email debe ser válido")
+    @NotEmpty(message = "El email no puede estar vacío")
+    @Column(name = "email")
     private String email;
+
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    @NotEmpty(message = "La contraseña no puede estar vacía")
+    @Column(name = "contrasenia")
     private String password;
+
+    @Column(name = "rol")
     private Role role;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Car> publishedCars;
+
+    @ManyToMany
+    @JoinTable(
+            name = "usuario_lista_deseados",
+            joinColumns = @JoinColumn(name = "id_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "id_vehiculo")
+    )
+    private List<Car> wishlist;
 
 }

9e393c274b262d4bb223b11e2d221c535368046a
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-30 18:08:48 -0600

    Implemented Enum Role

--- src/main/java/com/seminuevos_colima/entity/Role.java
+++ src/main/java/com/seminuevos_colima/entity/Role.java
@@ -2,6 +2,18 @@ package com.seminuevos_colima.entity;
 
 public enum Role {
 
-    SELLER, BUYER, ADMIN
+    SELLER("Seller"),
+    BUYER("Buyer"),
+    ADMIN("Administrator");
+
+    private final String displayName;
+
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 
 }
